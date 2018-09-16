06b8a303ca7fe7b7541f8ec4d9f4693ceb6b76de
Stef Claus <stef.claus@gmail.com>
2018-09-16 21:59:20 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,9 @@ def key_for_min_value(name_hash)
   holding_value = nil 
   name_hash.each do |key, value|
     binding.pry 
-  
+    holding_key = key 
+    holding_value = value
+    
   end 
   holding_key
 end
